b4d54712cedf4cd371e9b5a4c7812c3cdb3647b9
AKSAR <akshatjaiswal2518@gmail.com>
2025-02-23 13:49:16 +0530

    some modification

--- client/src/pages/SetupList.jsx
+++ client/src/pages/SetupList.jsx
@@ -32,18 +32,7 @@ const SetupList = () => {
       }
     };
 
-    const getTags = async () => {
-      try {
-        const response = await axios.get(`${API_URL}/tags`);
-        setTags(response.data.data);
-        // console.log(response.data.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     getSetups();
-    getTags();
   }, [page, statusFilter, tagFilter, searchQuery]);
 
   useEffect(() => {
